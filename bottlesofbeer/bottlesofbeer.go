1a0c07ad3ec6bd50b0d93a2c68695b9e9cd53879
victorkingi <kingivictor77@gmail.com>
2020-11-26 18:28:55 +0000

    changes

--- bottlesofbeer/bottlesofbeer.go
+++ bottlesofbeer/bottlesofbeer.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	//	"net/rpc"
-	//	"fmt"
-	//	"time"
 	"flag"
 	"fmt"
 	"net"
@@ -11,21 +8,16 @@ import (
 	"time"
 )
 
-var nextAddr string
 var initialised = false
 var nextround *rpc.Client
-
-type BottlesOfBeer struct{}
-type Token struct {
-	Bottles int
-}
+var nextAddr string
 
 func Beers(i int) {
-	time.Sleep(2 * time.Second)
+	time.Sleep(1 * time.Second) //Delay just so the round is visible
 	if i > 1 {
 		fmt.Printf("%v bottles of beer on the wall, %v bottles of beer\nTake one down, pass it around...", i, i)
 	} else if i == 1 {
-		fmt.Printf("%v bottle of beer on the wall, %v bottle of beer\nTake one down, pass it around...", i, i)
+		fmt.Printf("One bottle of beer on the wall, one bottle of beer\nTake it down, pass it around...")
 	} else {
 		fmt.Printf("No more bottles of beer on the wall, no more bottles of beer\nGo to the store and buy some more!\n")
 	}
@@ -34,7 +26,6 @@ func Beers(i int) {
 func PassItAround(bottles int) {
 	reqs := Token{Bottles: bottles}
 	resp := new(Token)
-
 	if initialised == false {
 		nextround, _ = rpc.Dial("tcp", nextAddr)
 		initialised = true
@@ -42,28 +33,28 @@ func PassItAround(bottles int) {
 	nextround.Go("BottlesOfBeer.Round", reqs, resp, nil)
 }
 
-func (b *BottlesOfBeer) Round(inToken Token, outToken *Token) (err error) {
-	bottles := inToken.Bottles
-	Beers(bottles)
+type BottlesOfBeer struct{}
+type Token struct {
+	Bottles int
+}
 
+func (b *BottlesOfBeer) Round(intoken Token, outtoken *Token) (err error) {
+	bottles := intoken.Bottles
+	Beers(bottles)
 	if bottles > 0 {
 		PassItAround(bottles - 1)
 	}
-
 	return
 }
 
 func main() {
-	thisPort := flag.String("this", "8030", "Port for this process to listen on")
+	thisPort := flag.String("this", "8030", "Port to listen on")
 	flag.StringVar(&nextAddr, "next", "localhost:8040", "IP:Port string for next member of the round.")
-	bottles := flag.Int("n", 0, "Bottles of Beer (launches song if not 0)")
+	bottles := flag.Int("n", 0, "Bottles of Beer (launches round)")
 	flag.Parse()
-	//TODO: Up to you from here! Remember, you'll need to both listen for
-	//RPC calls and make your own.
 	rpc.Register(&BottlesOfBeer{})
 	listener, _ := net.Listen("tcp", ":"+*thisPort)
 	defer listener.Close()
-
 	if *bottles > 0 {
 		Beers(*bottles)
 		go PassItAround(*bottles - 1)
