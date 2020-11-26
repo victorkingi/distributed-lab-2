b455bb2c7d27a74f269424d36cdaa104a61739d2
victorkingi <kingivictor77@gmail.com>
2020-11-26 16:27:39 +0000

    changes

--- secretstrings/server/server.go
+++ secretstrings/server/server.go
@@ -1,23 +1,39 @@
-
 package main
+
 import (
-//	"errors"
-//	"flag"
-//	"fmt"
-//	"net"
-	"time"
+	"flag"
 	"math/rand"
-//	"secretstrings/stubs"
-//	"net/rpc"
+	"net"
+	"net/rpc"
+	"secretstrings/stubs"
+	//	"errors"
+	//	"fmt"
+	"time"
 )
 
 /** Super-Secret `reversing a string' method we can't allow clients to see. **/
 func ReverseString(s string, i int) string {
-    time.Sleep(time.Duration(rand.Intn(i))* time.Second)
-    runes := []rune(s)
-    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-        runes[i], runes[j] = runes[j], runes[i]
-    }
-    return string(runes)
+	time.Sleep(time.Duration(rand.Intn(i)) * time.Second)
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
 
+type SecretStringOperations struct{}
+
+func (s *SecretStringOperations) Reverse(req stubs.Request, res *stubs.Response) (err error) {
+	res.Message = ReverseString(req.Message, 10)
+	return
+}
+
+func main() {
+	pAddr := flag.String("port", "8010", "Port to listen on")
+	flag.Parse()
+	rand.Seed(time.Now().UnixNano())
+	rpc.Register(&SecretStringOperations{})
+	listener, _ := net.Listen("tcp", ":"+*pAddr)
+	defer listener.Close()
+	rpc.Accept(listener)
+}
